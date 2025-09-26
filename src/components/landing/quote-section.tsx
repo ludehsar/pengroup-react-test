7b2d5212e347e46f122140a528a2b994705ea84d
Rashedul Alam <rashedulalam112112@gmail.com>
2025-09-26 18:38:44 +0600

    Fix mobile UI for quote section

--- src/components/landing/quote-section.tsx
+++ src/components/landing/quote-section.tsx
@@ -4,7 +4,7 @@ import Button from "../ui/button";
 
 const QuoteSection = () => {
   return (
-    <section className="flex flex-col items-start justify-between gap-6 py-10 pb-20 lg:flex-row lg:gap-8 lg:py-20 lg:pb-[200px]">
+    <section className="flex flex-col items-start justify-between gap-6 py-10 pb-[200px] xl:flex-row xl:gap-8 xl:py-20">
       <figure className="mx-auto max-w-screen-md space-y-[40px]">
         <svg
           width="72"
@@ -19,7 +19,7 @@ const QuoteSection = () => {
           />
         </svg>
         <blockquote>
-          <p className="ml-4 w-full max-w-[535px] align-middle text-3xl leading-[100%] font-black tracking-[0%] sm:text-4xl md:text-5xl lg:ml-[72px] lg:text-[64px]">
+          <p className="ml-4 w-full max-w-[535px] align-middle text-3xl leading-[100%] font-black tracking-[0%] sm:text-4xl md:text-5xl xl:ml-[72px] xl:text-[64px]">
             A World where <span className="text-primary">EVERYONE has</span> the{" "}
             <span className="text-primary">opportunity</span> to fulfil their
             potential
@@ -31,7 +31,7 @@ const QuoteSection = () => {
           viewBox="0 0 71 55"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
-          className="mr-4 ml-auto lg:mr-[82px]"
+          className="mr-4 ml-auto xl:mr-[82px]"
         >
           <path
             d="M70.5316 0.984375V18.0298C70.5316 22.67 69.5846 27.3338 67.6907 32.0213C65.7967 36.7088 63.4056 41.0412 60.5174 45.0185C57.6291 48.9957 54.7172 52.2154 51.7816 54.6776L35.5884 46.4389C38.0505 42.3196 39.9681 38.0109 41.3412 33.5128C42.667 29.0147 43.3062 23.8537 43.2588 18.0298V0.984375H70.5316ZM35.0202 0.984375V18.0298C35.0202 22.67 34.0732 27.3338 32.1793 32.0213C30.2853 36.7088 27.8942 41.0412 25.006 45.0185C22.1177 48.9957 19.2058 52.2154 16.2702 54.6776L0.0770104 46.4389C2.53914 42.3196 4.45675 38.0109 5.82985 33.5128C7.15561 29.0147 7.79482 23.8537 7.74747 18.0298V0.984375H35.0202Z"
@@ -54,8 +54,8 @@ const QuoteSection = () => {
           width={130}
           height={130}
         />
-        <div className="text-primary absolute right-0 bottom-[-60px] z-10 flex w-full max-w-full flex-col items-center justify-between gap-4 bg-[#061665] px-4 py-8 lg:right-[-50px] lg:bottom-[-180px] lg:w-[852px] lg:flex-row lg:gap-0 lg:px-12 lg:py-16">
-          <div className="max-w-[430px] text-center align-middle text-xl leading-[100%] font-black tracking-[0%] sm:text-2xl lg:text-left lg:text-[40px]">
+        <div className="text-primary absolute right-0 bottom-[-160px] z-10 flex w-full max-w-full flex-col items-center justify-between gap-4 bg-[#061665] px-4 py-8 xl:right-[-50px] xl:bottom-[-180px] xl:w-[852px] xl:flex-row xl:gap-0 xl:px-12 xl:py-16">
+          <div className="max-w-[430px] text-center align-middle text-xl leading-[100%] font-black tracking-[0%] sm:text-2xl xl:text-left xl:text-[40px]">
             There&apos;s Still Time <br />
             to Apply for September 2025
           </div>
